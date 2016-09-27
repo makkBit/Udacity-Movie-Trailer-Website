a4ab3a0426db98423516a0a0239a3dc041f2d16b
Mahak <makk.bit@outlook.com>
2016-09-27 17:03:01 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,8 +1,8 @@
 # this module defines the class 'Movie',
 # to create objects in entertainment_center.py module
 class Movie():
-	# documentation of the class, accessed by ClassName.__doc__
-	'This class defines the blueprint of the movies'
+	"""documentation of the class, accessed by ClassName.__doc__
+	This class defines the blueprint of the movies"""
 	# constructor to create space in memory 
 	# for the instance of the class
 	def __init__(self,movie_title,poster_image,
