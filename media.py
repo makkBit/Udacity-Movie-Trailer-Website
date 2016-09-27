516ffc9a70f385eadb24b4e0a5adcd2d66fed899
Mahak <makk.bit@outlook.com>
2016-09-27 14:28:29 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,8 +1,10 @@
-import webbrowser
-
+# this module defines the class 'Movie',
+# to create objects in entertainment_center.py module
 class Movie():
 	# documentation of the class, accessed by ClassName.__doc__
 	'This class defines the blueprint of the movies'
+	# constructor to create space in memory 
+	# for the instance of the class
 	def __init__(self,movie_title,poster_image,
 				trailer_youtube,id_youtube):
 		# instance variables
